6706be039d53670c3cc01d2ce82d74396db5ec5e
Dan3s <daniel.guzman1@correo.icesi.edu.co>
2020-05-01 19:22:38 -0500

    Finish
    Miguel Romero
    Daniel Guzmán

--- 03-games/alpha_beta.py
+++ 03-games/alpha_beta.py
@@ -1,3 +1,4 @@
+
 def is_game_over(node):
     winning_indexes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -18,12 +19,21 @@ def is_game_over(node):
     return False, None
 
 def generate_children(node, chosen_symbol): # TODO: Create a function to generate the children states for minimax evaluation
-    pass
+    #generatethe
+    generarHijos = []
+
+    for indexHijos in range(len(node)):
+
+        if node[indexHijos] == None or node[indexHijos] == -1:
+            hijoGenerado = node.copy()
+            hijoGenerado[indexHijos] = chosen_symbol
+            generarHijos.append(hijoGenerado)
+    return generarHijos
 
 def alternate_symbol(symbol):
     return 'o' if symbol == 'x' else 'x'
 
-def mini_max_ab(node, is_maximizing_player_turn, chosen_symbol): # TODO: Modify this minimax in order to turn it into an alpha-beta pruning version with depth cutting
+def mini_max_ab(node, is_maximizing_player_turn, chosen_symbol, alpha, beta): # TODO: Modify this minimax in order to turn it into an alpha-beta pruning version with depth cutting
     game_result = is_game_over(node)
 
     if game_result[0]:
@@ -32,16 +42,43 @@ def mini_max_ab(node, is_maximizing_player_turn, chosen_symbol): # TODO: Modify 
 
         return (-1, node) if is_maximizing_player_turn else (1, node)
 
-    children = generate_children(node, chosen_symbol)
-    children_results = list(map(
-        lambda child: [
-            mini_max(child, not is_maximizing_player_turn, alternate_symbol(chosen_symbol))[0],
-            child
-        ],
-        children
-    ))
+    childrens = generate_children(node, chosen_symbol)
 
-    return max(children_results, key=str) if is_maximizing_player_turn else min(children_results, key=str)
+    if is_maximizing_player_turn:
+        PUNTUACION = -9999
+        unHijo = []
+
+        for hijo in childrens:
+            hijo2 = mini_max_ab(hijo, not is_maximizing_player_turn, alternate_symbol(chosen_symbol), alpha, beta)
+            if PUNTUACION < hijo2[0]:
+                unHijo = hijo
+
+            PUNTUACION = max(PUNTUACION, hijo2[0])
+            alpha = max(alpha, hijo2[0])
+            if beta <= alpha:
+                break
+
+        return PUNTUACION, unHijo
+
+
+    else:
+
+        PUNTUACIONBAJA = 9999
+        unHijo = []
+
+        for hijo in childrens:
+            hijo2 = mini_max_ab(hijo, not is_maximizing_player_turn, alternate_symbol(chosen_symbol), alpha, beta)
+            if PUNTUACIONBAJA > hijo2[0]:
+                unHijo = hijo
+
+            PUNTUACIONBAJA = min(PUNTUACIONBAJA, hijo2[0])
+            beta = min(beta, hijo2[0])
+            if beta <= alpha:
+               break
+
+        return PUNTUACIONBAJA, unHijo
+
+
 
 def mini_max(node, is_maximizing_player_turn, chosen_symbol):
     game_result = is_game_over(node)
